4b13913d0f9da982fff73ccc605c04d8537f8ba2
April <macuser@Mac-User.local>
2020-07-14 21:37:58 -0600

    complete signup and login pages

--- server.js
+++ server.js
@@ -8,13 +8,17 @@ const app = express();
 require('dotenv').config();
 require('./config/database');
 
+const usersRoutes = require('./routes/users');
+
 app.use(logger('dev'));
 app.use(express.json());
-
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Put API routes here, before the "catch all" route
+app.use('/api/users', usersRoutes);
+// Load config/auth
+app.use(require('./config/auth'));
 
 // The following "catch all" route (note the *)is necessary
 // for a SPA's client-side routing to properly work
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,26 +1,57 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import { Route, Switch, Redirect } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
+import userService from '../../utils/userService';
 
 class App extends Component {
+  state = {
+    resources: [],
+    user: userService.getUser(),
+  }
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({
+      user: null
+    }, () => this.props.history.push('/'));
+  }
+
+  handleSignupOrLogin = () => {
+    this.setState({
+      user: userService.getUser()
+    }, () => this.getAllResources());
+  }
+
+  async componentDidMount() {
+    // this.getAllResources();
+    this.setState(
+      console.log('set state here')
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          Sonrisa LLC
+          &nbsp;&nbsp;&nbsp;
           <nav className="NavBar">
-              <Switch>
-                <Route>
-                  <NavBar />
-                </Route>
-              </Switch>
+            <NavBar />
           </nav>
         </header>
         <main>
-          <p>
-            Sonrisa LLC
-          </p>
+          <Switch>
+            <Route exact path='/signup' render={({ history }) =>
+              <SignupPage history={history} handleSignupOrLogin={this.handleSignupOrLogin} />
+            } />
+            <Route exact path='/login' render={({ history }) =>
+              <LoginPage history={history} handleSignupOrLogin={this.handleSignupOrLogin} />
+            } />
 
+          </Switch>
         </main>
       </div>
     );
